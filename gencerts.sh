5de136440c7bc4ef3892bdedf01f0770c20d611e
Kyle Kelley <rgbkrk@gmail.com>
2016-03-24 16:51:36 -0500

    generate certificates programmatically

---
+++ gencerts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p certificates
+
+touch certificates/password
+chmod 600 certificates/password
+cat /dev/random | head -c 128 | base64 > certificates/password
+
+rm -f certificates/*.srl certificates/*.csr certificates/*.pem
+
+export KEYMASTER="docker run --rm -v $(pwd)/certificates/:/certificates/ cloudpipe/keymaster"
+
+${KEYMASTER} ca
+${KEYMASTER} signed-keypair -n beacon -h 127.0.0.1 -s IP:127.0.0.1 -p server
+${KEYMASTER} signed-keypair -n beacon-cli -h 127.0.0.1 -s IP:127.0.0.1 -p client
