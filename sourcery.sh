304d27ee05f6f1553064cfa5bc3e0f1a8a4bd8c5
Kyle Kelley <rgbkrk@gmail.com>
2016-03-25 10:29:05 -0500

    Remarkable copy pasta waste of 3 hours

--- sourcery.sh
+++ sourcery.sh
@@ -2,6 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/certificates"
 
 export CLIENT_CERT=`cat $DIR/client-cert.pem`
 export CLIENT_KEY=`cat $DIR/client-key.pem`
-export SERVER_CERT=`cat $DIR/client-cert.pem`
-export SERVER_KEY=`cat $DIR/client-key.pem`
+export SERVER_CERT=`cat $DIR/server-cert.pem`
+export SERVER_KEY=`cat $DIR/server-key.pem`
 export CA=`cat $DIR/ca.pem`
